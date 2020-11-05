dc438a4b09c0b8d32ab5f0631a2beb4f842f696a
Regan <regan_maher@outlook.com>
2020-11-04 18:21:12 -0600

    Initial Push, got the data to load into the website.

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+// from data.js
+var tableData = data;
+var tbody = d3.select("tbody");
+
+
+console.log(data);
+
+
+data.forEach((ufoSighting) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+// YOUR CODE HERE!
