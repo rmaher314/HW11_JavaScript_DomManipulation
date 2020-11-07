5c7d587df09878ce1d2dd5bb7f03f5487712da0c
Regan <regan_maher@outlook.com>
2020-11-07 15:05:45 -0600

    working on setting up the code to read in the date.

--- static/js/app.js
+++ static/js/app.js
@@ -3,9 +3,10 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 
+
 console.log(data);
 
-
+// Import data table onto website
 data.forEach((ufoSighting) => {
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(([key, value]) => {
@@ -13,4 +14,22 @@ data.forEach((ufoSighting) => {
       cell.text(value);
     });
   });
-// YOUR CODE HERE!
+
+//Select the botton
+var button = d3.select("#filter-btn");
+
+//Select the form
+var form = d3.select("#datetime");
+
+// Event Handlers
+button.on("click", runEnter);
+form.on("submit",runEnter); 
+
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node 
+    //It is either line 34 or 22 that is wrong.
+    var inputElement = d3.select("#datetime");
