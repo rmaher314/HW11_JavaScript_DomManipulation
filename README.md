5ae6e49c37d1d288b71adb321cc088f4c5cc830e
Regan <regan_maher@outlook.com>
2020-11-09 17:55:13 -0600

    Finished up the code and completed the read me doc.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # HW11_JavaScript_DomManipulation
+
+This assignment includes the following elementts needed for this assignment.  
+
+Static Folder Files:
+
+js
+1.  data.js - data file
+2.  app.js - code file
+3.  index.html - web code
+
+Couple of Items to Note:
+1.  Fed in a data reader to take in multiple date types and return the same data.
+2.  Added an enter function, mostly because it was driving me personally crazy that I coudl not just hit enter after I put in the date and had to click on the filter button.  
+
+
--- static/js/app.js
+++ static/js/app.js
@@ -77,3 +77,56 @@ function runEnter() {
           });
       }
 }
+// // //Select City Info
+// var button = d3.select("#filter-btn");
+
+// //Select the form
+// var form = d3.select("#City");
+
+// // Event Handlers
+// button.on("click", runEnter);
+// form.on("keypress",function() {
+//     if(d3.event.keyCode === 13){
+//         runEnter();
+//     }});
+  
+
+// function runEnter() {
+//     console.log("runEnter called")
+//     // Prevent the page from refreshing
+//     d3.event.preventDefault();
+    
+//     // Select the input element and get the raw HTML node 
+//     var inputElement = d3.select("#city");
+//     var inputValue = inputElement.property("value");
+   
+//     //todo convert inputValue to date format
+
+//     //clears out the ufo table
+//     var list = d3.select(".ufoTable");
+//     list.html("");
+
+//     var match = false;
+//     data.forEach((ufoSighting) => {       
+//         var row = tbody.append("tr");
+//         match = false;
+//         Object.entries(ufoSighting).forEach(([key, value]) => {
+//             if(key == "city")
+//             if (match == true){
+//                 var cell = row.append("td");
+//                 cell.text(value);
+                
+//             }
+//         });
+//       });
+      
+//       if(inputValue == null || inputValue == ""){
+//         data.forEach((ufoSighting) => {
+//             var row = tbody.append("tr");
+//             Object.entries(ufoSighting).forEach(([key, value]) => {
+//               var cell = row.append("td");
+//               cell.text(value);
+//             });
+//           });
+//       }
+// }
